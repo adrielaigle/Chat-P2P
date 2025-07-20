b8a83cf9af028259e4360b828e70e0747663a5db
Adriel Aigle <adriel.aigle2@hotmail.com>
2025-07-20 15:36:07 -0300

    segundo commit

--- public/client.js
+++ public/client.js
@@ -1,6 +1,8 @@
 const socket = io();
 let mySocketId = null;
 
+const peers = {}; 
+
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
@@ -32,7 +34,14 @@ form.addEventListener('submit', function(e) {
       message: convertEmojis(message),
       id: mySocketId
     };
-    socket.emit('chat message', data);
+
+    for (const peer of Object.values(peers)) {
+      if (peer.dataChannel.readyState === 'open') {
+        peer.dataChannel.send(JSON.stringify(data));
+      }
+    }
+
+    displayMessage(data); 
     input.value = '';
   }
 });
@@ -41,20 +50,91 @@ socket.on('connect', () => {
   mySocketId = socket.id;
 });
 
-socket.on('chat history', (history) => {
-  messages.innerHTML = '';
-  for (const data of history) {
-    displayMessage(data);
+socket.on('user-connected', async (peerId) => {
+  if (peerId === mySocketId) return;
+
+  const peer = createPeerConnection(peerId);
+  const dataChannel = peer.createDataChannel("chat");
+
+  setupDataChannel(peerId, dataChannel);
+
+  const offer = await peer.createOffer();
+  await peer.setLocalDescription(offer);
+
+  socket.emit('signal', {
+    to: peerId,
+    offer
+  });
+
+  peers[peerId] = { connection: peer, dataChannel };
+});
+
+socket.on('signal', async (data) => {
+  let peer = peers[data.from]?.connection;
+
+  if (!peer) {
+    peer = createPeerConnection(data.from);
+    peers[data.from] = { connection: peer };
+  }
+
+  if (data.offer) {
+    await peer.setRemoteDescription(new RTCSessionDescription(data.offer));
+    const answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+
+    socket.emit('signal', {
+      to: data.from,
+      answer
+    });
+
+  } else if (data.answer) {
+    await peer.setRemoteDescription(new RTCSessionDescription(data.answer));
+
+  } else if (data.ice) {
+    try {
+      await peer.addIceCandidate(new RTCIceCandidate(data.ice));
+    } catch (err) {
+      console.warn('Erro ao adicionar ICE:', err);
+    }
   }
 });
 
-socket.on('chat message', function(data) {
-  displayMessage(data);
+socket.on('user-disconnected', (peerId) => {
+  if (peers[peerId]) {
+    peers[peerId].connection.close();
+    delete peers[peerId];
+  }
 });
 
-socket.on('error message', (msg) => {
-  alert(msg);
-});
+function createPeerConnection(peerId) {
+  const peer = new RTCPeerConnection({
+    iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+  });
+
+  peer.onicecandidate = (event) => {
+    if (event.candidate) {
+      socket.emit('signal', {
+        to: peerId,
+        ice: event.candidate
+      });
+    }
+  };
+
+  peer.ondatachannel = (event) => {
+    const channel = event.channel;
+    setupDataChannel(peerId, channel);
+    peers[peerId].dataChannel = channel;
+  };
+
+  return peer;
+}
+
+function setupDataChannel(peerId, channel) {
+  channel.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    displayMessage(data);
+  };
+}
 
 function isMyMessage(data) {
   const myUsername = usernameInput.value.trim();
